1a35f3ae0fa3741954c6ff3698d4db809a5d1cf3
wilner2 <wilnerbrum@gmail.com>
2024-04-19 21:16:49 -0300

    test: return 400 if marca is not provided on RegisterAutoController

--- tests/app/controllers/registerAuto.spec.ts
+++ tests/app/controllers/registerAuto.spec.ts
@@ -20,6 +20,18 @@ describe("registerAuto Controller", () => {
       HTTPBadRequest(new ParamIsMissing("placa")).msg
     );
   });
+  test("should return 400 if placa is not provided", () => {
+    const request = {
+      placa: "any_placa",
+      cor: "any_cor",
+    };
+    const response = sut.handle(request);
+
+    expect(response.statusCode).toBe(400);
+    expect(response.msg).toEqual(
+      HTTPBadRequest(new ParamIsMissing("marca")).msg
+    );
+  });
 
   test("should return 200 if auto created successfully", () => {
     const request = {

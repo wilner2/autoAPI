bd1aeb8ddc162a06b213342cd8a0122aa9c0bc6b
wilner2 <wilner_brum@hotmail.com>
2024-04-20 19:15:28 -0300

    feat: create express automovel route

--- src/infra/conf/datasource.ts
+++ src/infra/conf/datasource.ts
@@ -10,7 +10,7 @@ export const AppDataSource = new DataSource({
     database: "postgres",
     schema: "public",
     synchronize: true,
-    logging: true,
+    logging: false,
     entities: [Auto],
     subscribers: [],
     migrations: [],
--- src/main/index.ts
+++ src/main/index.ts
@@ -1,8 +1,28 @@
 import "reflect-metadata"
-import express from 'express'
+import './config/configAlias'
+import express, { json } from 'express'
+import { RegisterAutoController } from "@/app/controllers/registerAuto"
+import { RegisterAutoCase } from "@/domain/useCases/registerAuto"
+import { AutoRepository } from "@/infra/repositories/autoRepo"
+import { AppDataSource } from "@/infra/conf/datasource"
 
+
+const autoRepository = new AutoRepository(AppDataSource)
+const registerAuto = new RegisterAutoCase(autoRepository)
+const controller = new RegisterAutoController(registerAuto)
 const app = express()
+app.use(json())
+app.use((req, res, next) => {
+    res.type('json')
+    next()
+})
 const port = 3000
+app.use
+app.post('/automovel', async (req, res) => {
+    console.log(req.body);
 
+    const result = await controller.handle(req.body)
+    res.status(result.statusCode).send(result)
+})
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))

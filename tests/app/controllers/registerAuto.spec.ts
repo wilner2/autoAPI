ae7c56a83afc95b716e6b0a9ac4ca5a724a5e024
wilner2 <wilnerbrum@gmail.com>
2024-04-19 21:15:11 -0300

    test: return 400 if placa is not provided on RegistterAutoController

--- tests/app/controllers/registerAuto.spec.ts
+++ tests/app/controllers/registerAuto.spec.ts
@@ -1,8 +1,27 @@
 import { RegisterAutoController } from "@/app/controllers/registerAuto";
+import { ParamIsMissing } from "@/helpers/excepetions";
+import { HTTPBadRequest } from "@/helpers/http";
 
 describe("registerAuto Controller", () => {
+  let sut: RegisterAutoController;
+  beforeEach(() => {
+    sut = new RegisterAutoController();
+  });
+
+  test("should return 400 if placa is not provided", () => {
+    const request = {
+      cor: "any_cor",
+      marca: "any_marca",
+    };
+    const response = sut.handle(request);
+
+    expect(response.statusCode).toBe(400);
+    expect(response.msg).toEqual(
+      HTTPBadRequest(new ParamIsMissing("placa")).msg
+    );
+  });
+
   test("should return 200 if auto created successfully", () => {
-    const sut = new RegisterAutoController();
     const request = {
       placa: "any_placa",
       cor: "any_cor",
@@ -12,6 +31,6 @@ describe("registerAuto Controller", () => {
     const response = sut.handle(request);
 
     expect(response.statusCode).toBe(200);
-    expect(response.msg).toBe("Automóvel criado com sucesso");
+    expect(response.msg).toEqual("Automóvel created successfully");
   });
 });

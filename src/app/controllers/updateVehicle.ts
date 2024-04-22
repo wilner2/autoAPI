06cdd11f131a68d36fbee530f3e418942201a1a8
wilner2 <wilner_brum@hotmail.com>
2024-04-22 09:52:03 -0300

    feat: return 404 if id of vehicle not exists on UpdateVehicleController

--- src/app/controllers/updateVehicle.ts
+++ src/app/controllers/updateVehicle.ts
@@ -1,6 +1,6 @@
 import Joi from "joi"
-import { ParamsError } from "@/app/helpers/excepetions";
-import { HTTPBadRequest, HTTPInternalServerError, HttpResponse, Ok } from "@/app/helpers/http";
+import { ParamsError, ResourceNotFound } from "@/app/helpers/excepetions";
+import { HTTPBadRequest, HTTPInternalServerError, HTTPNotFound, HttpResponse, Ok } from "@/app/helpers/http";
 import { UpdateVehicleContract } from "@/contracts/useCases/updateVehicle";
 import { Controller } from "./controller";
 
@@ -9,12 +9,15 @@ export class UpdateVehicleController implements Controller {
 
     async handle(request: any): Promise<HttpResponse> {
         try {
-
             const validation = this.validate(request)
             if (validation) {
                 return HTTPBadRequest(new ParamsError(validation));
             }
-            await this.updateVehicleCase.execute(request)
+
+            const response = await this.updateVehicleCase.execute(request)
+            if (response.vehicleNotFounded) {
+                return HTTPNotFound(new ResourceNotFound("ID not founded"))
+            }
             return Ok("created successfully");
         } catch (error) {
             return HTTPInternalServerError(error)

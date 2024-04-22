0bcbb7d14aa06418f17d208eb88e5a7ec106e282
wilner2 <wilner_brum@hotmail.com>
2024-04-22 17:35:27 -0300

    test: create delete use case

--- tests/domain/useCases/driver/deleteDriver.spec.ts
+++ tests/domain/useCases/driver/deleteDriver.spec.ts
@@ -1,34 +1,35 @@
 import { ExistsDriver, UpdateDriver } from "@/domain/contracts/repos/driver";
-import { UpdateDriverCase } from "@/domain/useCases/driver";
+import { DeleteDriverCase } from "@/domain/useCases/driver";
 import { mock, MockProxy } from "jest-mock-extended";
 
-describe('Update Driver Usecase', () => {
-    let sut: UpdateDriverCase
+describe('Delete Driver Usecase', () => {
+    let sut: DeleteDriverCase
     let stubUpdateDriver: MockProxy<UpdateDriver>
     let stubExistsDriver: MockProxy<ExistsDriver>
-    const request = { nome: "any_nome", id: 1, status: true }
     beforeEach(() => {
         stubUpdateDriver = mock<UpdateDriver>()
         stubExistsDriver = mock<ExistsDriver>()
-        sut = new UpdateDriverCase(stubUpdateDriver, stubExistsDriver)
+        sut = new DeleteDriverCase(stubUpdateDriver, stubExistsDriver)
     })
     test('should call update driver on repository', async () => {
         stubExistsDriver.exists.mockResolvedValueOnce(true)
-        const spyDriverRepository = jest.spyOn(stubUpdateDriver, "update")
+        const request = { id: "1" }
+        const spydriverRepository = jest.spyOn(stubUpdateDriver, "update")
         const response = await sut.execute(request)
 
-        expect(spyDriverRepository).toHaveBeenCalledTimes(1)
-        expect(spyDriverRepository).toHaveBeenCalledWith({ nome: "any_nome", id: 1, status: true })
+        expect(spydriverRepository).toHaveBeenCalledTimes(1)
+        expect(spydriverRepository).toHaveBeenCalledWith({ id: parseInt(request.id), status: false })
         expect(response).toEqual({ driverNotFounded: false })
     });
 
 
     test('should verify if driver exists in database', async () => {
+        const request = { id: "1" }
         const spydriverRepository = jest.spyOn(stubExistsDriver, "exists")
         const response = await sut.execute(request)
 
         expect(spydriverRepository).toHaveBeenCalledTimes(1)
-        expect(spydriverRepository).toHaveBeenCalledWith(request.id)
+        expect(spydriverRepository).toHaveBeenCalledWith(parseInt(request.id))
         expect(response).toEqual({ driverNotFounded: true })
     });
 

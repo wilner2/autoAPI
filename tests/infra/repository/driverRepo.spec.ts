6582099276a4beaad65a2b3736b9771fa49cf38b
wilner2 <wilner_brum@hotmail.com>
2024-04-22 16:44:01 -0300

    test: create driver on database

---
+++ tests/infra/repository/driverRepo.spec.ts
@@ -0,0 +1,86 @@
+import { DriverModel } from "@/infra/entities";
+import { DriverRepository } from "@/infra/repositories";
+import { DataType, IMemoryDb, newDb } from "pg-mem";
+import { DataSource } from "typeorm";
+import MockDate from 'mockdate'
+
+
+describe('Create Driver repository', () => {
+    let dataSource: DataSource
+    let db: IMemoryDb
+    let sut: DriverRepository
+    const request = { nome: 'any_nome' }
+    afterAll(() => {
+        MockDate.reset()
+    })
+    beforeAll(async () => {
+        MockDate.set(new Date().toString())
+        db = newDb()
+
+        db.public.registerFunction({
+            name: 'current_database',
+            args: [],
+            returns: DataType.text,
+            implementation: () => 'hello world: '
+        })
+        db.public.registerFunction({
+            name: 'version',
+            args: [],
+            returns: DataType.text,
+            implementation: () => 'hello world: '
+        })
+        db.public.registerFunction({
+            name: 'now',
+            args: [],
+            returns: DataType.timestamp,
+            implementation: () => 'hello world: '
+        })
+
+        db.public.registerFunction({
+            name: 'exists',
+            args: [DataType.integer],
+            returns: DataType.bool,
+            implementation: () => 'hello world: '
+        })
+
+        db.public.registerFunction({
+            name: "obj_description",
+            args: [DataType.text, DataType.text],
+            returns: DataType.text,
+            implementation: () => "test",
+        });
+
+
+        dataSource = db.adapters.createTypeormDataSource({
+            type: 'postgres',
+            port: 6566,
+            entities: [DriverModel],
+            logging: false,
+        })
+        await dataSource.initialize()
+        await dataSource.synchronize()
+        sut = new DriverRepository(dataSource)
+    })
+
+    /// create
+    test('should call getRepository', async () => {
+        const getRepositorySpy = jest.spyOn(sut, 'getRepository')
+        await sut.create(request)
+
+        expect(getRepositorySpy).toHaveBeenCalledWith(DriverModel)
+        expect(getRepositorySpy).toHaveBeenCalledTimes(1)
+    });
+
+    test('should save Driver in database', async () => {
+        const repository = jest.spyOn(sut.getRepository(DriverModel), 'save')
+        await sut.create(request)
+
+        expect(repository).toHaveBeenCalledWith({ ...request, created_at: new Date().toISOString(), status: true, id: 2 })
+        expect(repository).toHaveBeenCalledTimes(1)
+    });
+
+
+
+
+
+});

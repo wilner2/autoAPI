032ad1820978244f1799d9bce424c7ae362d08c6
wilner2 <wilner_brum@hotmail.com>
2024-04-22 10:29:37 -0300

    feat: return 404 if vehicle not exists in database

--- src/app/controllers/deleteVehicle.ts
+++ src/app/controllers/deleteVehicle.ts
@@ -1,22 +1,26 @@
-import { ParamsError } from "@/app/helpers/excepetions";
+import { ParamsError, ResourceNotFound } from "@/app/helpers/excepetions";
 import {
     HTTPBadRequest,
     HTTPInternalServerError,
+    HTTPNotFound,
     Ok,
 } from "@/app/helpers/http";
 import Joi from "joi";
 import { Controller } from "./controller";
-import { DeleteVehicle } from "@/contracts/useCases/deleteVehicle";
+import { DeletingVehicle } from "@/contracts/useCases/deleteVehicle";
 
 export class DeleteVehicleController implements Controller {
-    constructor(private deleteVehicle: DeleteVehicle) { }
+    constructor(private deleteVehicle: DeletingVehicle) { }
     async handle(request: any) {
         try {
             const validation = this.validate(request)
             if (validation) {
                 return HTTPBadRequest(new ParamsError(validation));
             }
-            await this.deleteVehicle.execute(request);
+            const response = await this.deleteVehicle.execute(request)
+            if (response.vehicleNotFounded) {
+                return HTTPNotFound(new ResourceNotFound("ID not founded"))
+            }
             return Ok("Deleted successfully");
         } catch (error) {
             return HTTPInternalServerError(error);

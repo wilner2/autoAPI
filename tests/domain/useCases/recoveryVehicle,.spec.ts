0291179d890e9dd1c342f85656e2acc392a96ee8
wilner2 <wilner_brum@hotmail.com>
2024-04-22 11:25:08 -0300

    test: create useCase that change vehicle status to true

---
+++ tests/domain/useCases/recoveryVehicle,.spec.ts
@@ -0,0 +1,37 @@
+import { ExistsVehicle, UpdateVehicle } from "@/contracts/repos/vehicle";
+import { RecoveryVehicleCase } from "@/domain/useCases/recoveryVehicle";
+import { mock, MockProxy } from "jest-mock-extended";
+
+describe('UpdateAuto Usecase', () => {
+    let sut: RecoveryVehicleCase
+    let stubUpdateVehicle: MockProxy<UpdateVehicle>
+    let stubExistsVehicle: MockProxy<ExistsVehicle>
+    beforeEach(() => {
+        stubUpdateVehicle = mock<UpdateVehicle>()
+        stubExistsVehicle = mock<ExistsVehicle>()
+        sut = new RecoveryVehicleCase(stubUpdateVehicle, stubExistsVehicle)
+    })
+    test('should call update Vehicle on repository', async () => {
+        stubExistsVehicle.exists.mockResolvedValueOnce(true)
+        const request = { id: "1" }
+        const spyVehicleRepository = jest.spyOn(stubUpdateVehicle, "update")
+        const response = await sut.execute(request)
+
+        expect(spyVehicleRepository).toHaveBeenCalledTimes(1)
+        expect(spyVehicleRepository).toHaveBeenCalledWith({ id: parseInt(request.id), status: true })
+        expect(response).toEqual({ vehicleNotFounded: false })
+    });
+
+
+    test('should verify if vehicle exists in database', async () => {
+        const request = { id: "1" }
+        const spyAutoRepository = jest.spyOn(stubExistsVehicle, "exists")
+        const response = await sut.execute(request)
+
+        expect(spyAutoRepository).toHaveBeenCalledTimes(1)
+        expect(spyAutoRepository).toHaveBeenCalledWith(parseInt(request.id))
+        expect(response).toEqual({ vehicleNotFounded: true })
+    });
+
+
+});

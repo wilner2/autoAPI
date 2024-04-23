d950870d12b864799c5b19d1ff85dc21fe4ba5b0
wilner2 <wilner_brum@hotmail.com>
2024-04-22 21:35:03 -0300

    feat: create finish records use case

--- src/domain/useCases/record/finishRecord.ts
+++ src/domain/useCases/record/finishRecord.ts
@@ -2,9 +2,9 @@ import { FinishRecord } from "@/domain/contracts/repos/record";
 import { FinishingRecord } from "@/domain/contracts/useCases/record";
 
 export class finishRecordCase implements FinishingRecord {
-    constructor(private finishrecord: FinishRecord) { }
+    constructor(private finishRecord: FinishRecord) { }
 
     async execute(record: FinishingRecord.Input): Promise<void> {
-        await this.finishrecord.update({ id: record.id, inProgress: false })
+        await this.finishRecord.update({ id: record.id, inProgress: false })
     }
 }

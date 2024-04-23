167e539c8ab15fd6f8d058bf01843e3509f11b80
wilner2 <wilner_brum@hotmail.com>
2024-04-22 21:38:52 -0300

    feat: create update function on record repo

--- src/infra/repositories/recordRepo.ts
+++ src/infra/repositories/recordRepo.ts
@@ -1,9 +1,9 @@
 import { RecordModel } from "@/infra/entities";
-import { CreateRecord, RecordInProgress } from "@/domain/contracts/repos/record";
+import { CreateRecord, RecordInProgress, FinishRecord } from "@/domain/contracts/repos/record";
 import { PgRepository } from "./repository";
 
 
-export class RecordRepository extends PgRepository implements CreateRecord, RecordInProgress {
+export class RecordRepository extends PgRepository implements CreateRecord, RecordInProgress, FinishRecord {
 
     async create({ idAutomovel, idMotorista, desc }: CreateRecord.Input): Promise<void> {
         const repository = this.getRepository(RecordModel)
@@ -13,5 +13,10 @@ export class RecordRepository extends PgRepository implements CreateRecord, Reco
     async findRecordInProgress({ idAutomovel, idMotorista }: RecordInProgress.Input): Promise<boolean> {
         const repository = this.getRepository(RecordModel)
         return await repository.exists({ where: [{ inProgress: true, idAutomovel }, { inProgress: true, idMotorista }] })
+    }
+
+    async update({ id, inProgress }: FinishRecord.Input): Promise<void> {
+        const repository = this.getRepository(RecordModel)
+        await repository.update({ id: id }, { inProgress })
     }
 } 
